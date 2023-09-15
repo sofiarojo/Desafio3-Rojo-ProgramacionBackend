46f2414f848688322f82942ea9c28f750fa5c22c
Sofia Rojo <sofiarojo27@gmail.com>
2023-09-15 14:10:43 -0300

    Desafio 3 - Problema ID

---
+++ src/ProductManager.js
@@ -0,0 +1,75 @@
+import { promises as fsPromises } from 'fs';
+
+class ProductManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    async getProducts() {
+        try {
+            const info = await fsPromises.readFile(this.path, 'utf-8');
+            return JSON.parse(info);
+        } catch (error) {
+        return error;
+        }
+    }
+
+    async addProduct(productData) {
+        try {
+            const products = await this.getProducts();
+            let id = 1;
+            if (products.length > 0) {
+            id = Math.max(...products.map((product) => product.id)) + 1;
+        }
+
+        const product = { id, ...productData };
+        products.push(product);
+        await fsPromises.writeFile(this.path, JSON.stringify(products));
+        return product;
+        } catch (error) {
+            return error;
+        }
+    };
+
+    async getProductsById(productId) {
+        try {
+            const products = await this.getProducts();
+            const product = products.find((p) => p.id === productId);
+            if (product) {
+            return product;
+            } else {
+            return "No product";
+            }
+            } catch (error) {
+                return error;
+        }
+    }
+
+  async updateProduct(productId, updateData) {
+    try {
+      const products = await this.getProducts();
+      const index = products.findIndex((p) => p.id === productId);
+      if (index !== -1) {
+        products[index] = { ...products[index], ...updateData };
+        await fsPromises.writeFile(this.path, JSON.stringify(products));
+        return products[index];
+      } else {
+        return "Product not found";
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async deleteProduct(productId) {
+    try {
+      const products = await this.getProducts();
+      const newProducts = products.filter((p) => p.id !== productId);
+      await fsPromises.writeFile(this.path, JSON.stringify(newProducts));
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default ProductManager;
---
+++ src/app.js
@@ -0,0 +1,46 @@
+//Desafio de la Clase 4
+import express from 'express';
+import ProductManager from './ProductManager.js';
+
+
+const app = express();
+
+const productManager = new ProductManager('../products.json');
+
+app.use(express.json());
+
+//Endpoint para obtener todos los productos con límite opcional
+app.get('/products', async (req, res) => {
+    try {
+        const {limit} = req.query;
+        const products = await productManager.getProducts();
+
+        if(limit) {
+            const limitedProducts = products.slice(0, parseInt(limit));
+            res.json({ products: limitedProducts });
+        }else {
+            res.json({ products });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los productos '});
+    }
+});
+
+//Endpoint para obtener un producto por ID
+app.get('/products/:pid', async (req, res) => {
+    const {productId} = req.params;
+    try {
+        const product = await productManager.getProductsById(productId);
+
+        if (!product) {
+            return res.status(400).json({ message: "Product not found with the id" });
+        }
+        res.status(200).json({ message: "Product found", product });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.listen(8080, () => {
+    console.log('¡Escuchando servidor 8080!');
+});
